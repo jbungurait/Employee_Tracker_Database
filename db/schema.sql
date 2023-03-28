47ce14207ae7213ff6c6dc195a49704e7a91ef20
jbungurait <120280286+jbungurait@users.noreply.github.com>
2023-03-27 21:28:51 -0700

    ok databases set up now inquirer

--- db/schema.sql
+++ db/schema.sql
@@ -3,18 +3,8 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE employee_list (
-    id INT AUTO_INCREMENT NOT NULL,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    manager TEXT,
-    role_id INT NOT NULL,
-    FOREIGN KEY (role_id)
-    REFERENCES roles(id),
-    PRIMARY KEY (id),
-);
 
-CREATE TABLE dapartment (
+CREATE TABLE department (
     id INT NOT NULL,
     PRIMARY KEY (id),
     dept_name VARCHAR(30) NOT NULL
@@ -22,10 +12,21 @@ CREATE TABLE dapartment (
 
 CREATE TABLE roles (
     id INT NOT NULL,
-    title VARCHAR(30) NOT NULL,
+    title TEXT NOT NULL,
     salary INT NOT NULL,
     PRIMARY KEY (id),
     department_id INT NOT NULL,
     FOREIGN KEY (department_id)
-    REFERENCES dapartment(id)
+    REFERENCES department(id)
+);
+
+CREATE TABLE employees (
+    id INT AUTO_INCREMENT NOT NULL,
+    PRIMARY KEY(id),
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager TEXT,
+    FOREIGN KEY(role_id)
+    REFERENCES roles(id)
 );
--- server.js
+++ server.js
@@ -3,23 +3,4 @@ const inquirer = require('inquirer');
 
 const runExec = new Executable();
 
-inquirer
-    .prompt([
-        {
-            type: list,
-            message: 'Welcome to Employee Database. What would you like to do?',
-            choices: ['View all Employees', 'View all Departments', 'View all Roles', 'Add an Employee', 'Add a Department', 'Add a Role', 'Update Employee Role'],
-        }
-    ]).then((response) => {
-        if (response === 'View all Employees') {
-            runExec.getEmployess();
-        } else if (response === 'View all Departments') {
-            runExec.getDepartment();
-        } else if (response === 'View all Roles') {
-            runExec.getRoles();
-        } else if (response === 'Add an Employee') {
-            runExec.addEmployee();
-        }
-    });
-
-
+runExec.initialize();
